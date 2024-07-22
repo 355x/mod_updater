c61c7d3f5369435407d877afdae2ee97df30fd53
3_5_5 <355wastaken@proton.me>
2024-07-22 23:21:06 +0200

    added support for multiple modpacks

--- mod.py
+++ mod.py
@@ -6,25 +6,36 @@ import pathlib
 
 
 def update_all():
-    with open('config.yaml', 'r') as config:
-        config = yaml.safe_load(config)
-    mods = config['mods']
-    versions = config['versions']
-    newest = 'nope'
-    config['path'] = config['path'].replace('CURRENT', str(pathlib.Path(__file__).parent.resolve()))
-    with requests.get(f"https://api.modrinth.com/v2/project/fabric-api") as req:
-        if not req.ok:
-            print(f"Unable to get newest version")
+    home = str(pathlib.Path(__file__).parent.resolve())
+    for entry in os.listdir(f'{home}/config'):
+        if not entry.endswith('.yaml'):
+            print(f"Skipping non-YAML file: {entry}")
+            continue
+        try:
+            path = os.path.join(f'{home}/config', entry)
+            print(f'Updating modpack {entry.split('.')[0]}')
+            with open(path, 'r') as config:
+                config = yaml.safe_load(config)
+            mods = config['mods']
+            versions = config['versions']
+            newest = 'nope'
+            config['path'] = config['path'].replace('CURRENT', home)
+            with requests.get(f"https://api.modrinth.com/v2/project/fabric-api") as req:
+                if not req.ok:
+                    print(f"Unable to get newest version")
 
-        j = json.loads(req.content)
-    for i in j['game_versions']:
-        if i.startswith('1.'):
-            if not 'pre' in i and not 'rc' in i:
-                newest = i
+                j = json.loads(req.content)
+            for i in j['game_versions']:
+                if i.startswith('1.'):
+                    if not 'pre' in i and not 'rc' in i:
+                        newest = i
 
-    versions = [[newest, item[1]] if 'NEWEST' in item else item for item in versions]
-    mods, old = update(mods, versions, get_old(config['path']))
-    download(mods, config['path'], old)
+            versions = [[newest, item[1]] if 'NEWEST' in item else item for item in versions]
+            mods, old = update(mods, versions, get_old(config['path']))
+            download(mods, config['path'], old)
+        except Exception as e:
+            print(f'Encountered Error while updating {entry}:\n{e}.'
+                  f'\n Please check your configuration file and open an issue if the problem persists.')
 
 
 def update(mods: list, aversions: list, old: list):
@@ -36,11 +47,9 @@ def update(mods: list, aversions: list, old: list):
                 print(f"{mod} not found!")
                 continue
             j = json.loads(req.content)
-            print(j[0])
             for i in j:
                 for version in versions:
                     if version[0] in i['game_versions']:
-                        print(version)
                         if version[1] in i['loaders']:
                             versions.remove(version)
                             if not i['files'][0]['filename'] in old[0]:
@@ -81,13 +90,13 @@ def download(mods, path, old) -> None:
 def get_old(path) -> list:
     f = []
     p = []
-    print(f'searching {path} for currently installed mods...')
+    print(f'Searching {path} for currently installed mods...')
     for (dirpath, dirnames, filenames) in os.walk(path):
         for file in filenames:
             if file.endswith('.jar'):
                 f.append(file)
                 p.append(dirpath)
-    print(f'search complete! Found {len(f)} installed mods.')
+    print(f'Search complete! Found {len(f)} installed mods.')
     return [f, p]
 
 
